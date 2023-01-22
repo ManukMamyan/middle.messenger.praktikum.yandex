978d1e8e9024e25c2c954a2922d146331db24af2
Mamyan Manuk <manuk20061984@yandex.ru>
2023-01-23 00:20:14 +0300

    chore: revert

--- src/api/apiRequests.ts
+++ src/api/apiRequests.ts
@@ -1,19 +1,15 @@
-// import transport from './httpTransport';
+import transport from './httpTransport';
 
 const API_ENDPOINT = 'https://ya-praktikum.tech/api/v2';
 
 export function request<T extends any>({ method, path, data }: any): Promise<T> {
-  return fetch(`${API_ENDPOINT}/${path}`, {
+  return transport.request(`${API_ENDPOINT}/${path}`, {
     method,
     credentials: 'include',
     mode: 'cors',
     headers: { 'Content-Type': 'application/json' },
-    body: data ? JSON.stringify(data) : null,
-  }).then((resp) => {
-    return resp.json().catch(() => {
-      return Promise.resolve(resp)
-    });
-  })
+    data: data ? data : null,
+  });
 }
 
 request.post = <T>(path: string, data?: any) => request<T>({ method: 'POST', path, data });
